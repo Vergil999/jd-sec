5db007d772e21f3d52e715d83e56f677bc15add1
yongqi <lyq7845@126.com>
2022-02-18 17:47:49 +0800

    s

---
+++ build-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /root/github/jd-sec
+git pull origin master
+echo '-------------拉取最新代码over--------------'
+echo '-------------打包镜像--------------'
+docker build -t liyongqi666/jd-sec:1.0 .
+echo '-------------打包完毕--------------'
+
+
